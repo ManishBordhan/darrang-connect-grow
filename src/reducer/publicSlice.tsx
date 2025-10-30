a59bc06752562a515dd3643738ee9c2ebf0cacad
classic-creator <himangshukhound322@gmail.com>
2025-10-31 02:01:09 +0530

    test commit

--- src/reducer/publicSlice.tsx
+++ src/reducer/publicSlice.tsx
@@ -19,6 +19,8 @@ export const fetchAllEvents = createAsyncThunk(
           error.response?.data ||
           { message: error.message }
         );
+
+        
       }
       return rejectWithValue({ message: 'Unknown error occurred' });
     }
